8f3603161f36b66ac580851fcea6c560f9405e3e
harshadpatil6851 <harshadpatil6851@gmail.com>
2025-12-13 10:19:27 +0530

    Assignment02

---
+++ Assignment02/day2_4.py
@@ -0,0 +1,16 @@
+def prime(num):
+    if num <= 1:
+        return False
+    for i in range(2, int(num**0.5) + 1):
+        if num % i == 0:
+            return False
+    return True
+
+
+start = int(input("Enter starting number: "))
+end = int(input("Enter ending number: "))
+
+print("Prime numbers in the given range are:")
+for num in range(start, end + 1):
+        if prime(num):
+            print(num, end=" ")
---
+++ Assignment02/day2_5.py
@@ -0,0 +1,20 @@
+def print_binary(n):
+    print(bin(n)[2:])
+
+num = int(input("Enter a number: "))
+print_binary(num)
+
+def print_binary(n):
+    if n == 0:
+        print(0)
+        return
+
+    binary = ""
+    while n > 0:
+        binary = str(n % 2) + binary
+        n //= 2
+
+    print(binary)
+
+num = int(input("Enter a number: "))
+print_binary(num)
